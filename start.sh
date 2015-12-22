322a69c621bb0384c9b57e0aba9185d93eed17fd
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-22 18:23:16 -0500

    envified code

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export NODE_PATH=node_modules:.
+export NODE_ENV=development
 
 nodemon --exec babel-node -- index.node.js &
-watchify -e index.web.js -t babelify -o bundle.js -v
+watchify -e index.web.js -t babelify -g envify -o bundle.js -v
