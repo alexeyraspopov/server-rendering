c9fdde881374627257a1b4429f0cf03ade485db4
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-22 18:46:21 -0500

    moved restoring logic to actions

--- actions/CounterActions.js
+++ actions/CounterActions.js
@@ -3,3 +3,9 @@ import Dispatcher from 'lib/Dispatcher';
 export function increment() {
 	Dispatcher.dispatch({type: 'INCREMENTED'});
 }
+
+export function restore(state) {
+	if (state) {
+		Dispatcher.dispatch({type: 'COUNTER_RESTORED', value: parseInt(state)});
+	}
+}
--- index.web.js
+++ index.web.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import CounterStore from 'stores/CounterStore';
-import Dispatcher from 'lib/Dispatcher';
+import * as CounterActions from 'actions/CounterActions';
 
 ReactDOM.render(<App />, document.querySelector('main'));
 
 CounterStore.addListener(() => localStorage.setItem('counter', CounterStore.getState()));
-
-const restoredState = localStorage.getItem('counter');
-
-if (restoredState) {
-	Dispatcher.dispatch({type: 'COUNTER_RESTORED', value: restoredState});
-}
+CounterActions.restore(localStorage.getItem('counter'));
