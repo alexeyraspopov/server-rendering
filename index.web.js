78454dc0e9c82c752eb60294fe5ac402fb3a2b0e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-27 17:52:16 -0500

    removed unnecessary functionality

--- index.web.js
+++ index.web.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
-import CounterStore from 'stores/CounterStore';
-import * as CounterActions from 'actions/CounterActions';
 
 ReactDOM.render(<App />, document.querySelector('main'));
-
-CounterStore.addListener(() => localStorage.setItem('counter', CounterStore.getState()));
-CounterActions.restore(localStorage.getItem('counter'));
