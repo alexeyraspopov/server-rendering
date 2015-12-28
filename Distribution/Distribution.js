4c96244233d646d45f0d3064678c64fa1d32a813
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-27 21:06:06 -0500

    removed multiline

--- Distribution/Distribution.js
+++ Distribution/Distribution.js
@@ -8,11 +8,7 @@ export default function Distribution({values}) {
 	const hist = histogram().domain(abscisa.domain()).thresholds(abscisa.ticks(20));
 	const bins = hist(values);
 	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([120, 0]);
-	const p = area()
-		.x(b => abscisa(b.x0))
-		.y1(b => ordinata(b.length))
-		.y0(120)
-		.curve(basis);
+	const p = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(120).curve(basis);
 
 	return (
 		<article>
