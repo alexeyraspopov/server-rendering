52f545035cd06beac7adc731a40068a2a561010d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-27 21:02:35 -0500

    added simple distribution chart

--- App/App.js
+++ App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CounterContainer from 'Counter/CounterContainer';
+import Distribution from 'Distribution/Distribution';
+import {range} from 'd3-array';
+import {bates} from 'd3-random';
 
 export default function App() {
+	const values = range(0, 1000).map(bates(10));
+
 	return (
 		<section>
-			<h1>Hello</h1>
+			<Distribution values={values} />
 			<CounterContainer />
 		</section>
 	);
---
+++ Distribution/Distribution.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {histogram, max} from 'd3-array';
+import {linear} from 'd3-scale';
+import {area, basis} from 'd3-shape';
+
+export default function Distribution({values}) {
+	const abscisa = linear().domain([0, 1]).range([0, 300]);
+	const hist = histogram().domain(abscisa.domain()).thresholds(abscisa.ticks(20));
+	const bins = hist(values);
+	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([120, 0]);
+	const p = area()
+		.x(b => abscisa(b.x0))
+		.y1(b => ordinata(b.length))
+		.y0(120)
+		.curve(basis);
+
+	return (
+		<article>
+			<svg width={300} height={120}>
+				<path fill="rgba(0, 0, 255, 0.6)" d={p(bins)} />
+			</svg>
+		</article>
+	);
+}
