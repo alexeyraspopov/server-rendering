934d627391015b668c7c9f462ae4591187abfd4e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-28 15:26:42 -0500

    added ESLint with Airbnb config

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -4,6 +4,7 @@ export NODE_ENV=production
 rm -rf bundle
 mkdir bundle
 
+npm run lint
 browserify -e index.node.js -t babelify -g envify --bare -o bundle/server.js
 browserify -r react -r react-dom -g envify -o bundle/vendor.js
 browserify -e index.web.js -x react -x react-dom -t babelify -g envify -o bundle/client.js
