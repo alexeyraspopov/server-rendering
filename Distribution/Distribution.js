4490add705f2dc9bb027afed70a68eff213bfb21
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-27 22:55:38 -0500

    used constants in <Distribution>

--- Distribution/Distribution.js
+++ Distribution/Distribution.js
@@ -4,15 +4,17 @@ import {linear} from 'd3-scale';
 import {area, basis} from 'd3-shape';
 
 export default function Distribution({values}) {
-	const abscisa = linear().domain([0, 1]).range([0, 300]);
+	const WIDTH = 300;
+	const HEIGHT = 120;
+	const abscisa = linear().domain([0, 1]).range([0, WIDTH]);
 	const hist = histogram().domain(abscisa.domain()).thresholds(abscisa.ticks(20));
 	const bins = hist(values);
-	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([120, 0]);
-	const distribution = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(120).curve(basis);
+	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([HEIGHT, 0]);
+	const distribution = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(HEIGHT).curve(basis);
 
 	return (
 		<article>
-			<svg width={300} height={120}>
+			<svg width={WIDTH} height={HEIGHT}>
 				<path fill="rgba(0, 0, 255, 0.6)" d={distribution(bins)} />
 			</svg>
 		</article>
