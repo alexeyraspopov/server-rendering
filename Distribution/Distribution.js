473c9e6594e8a4f315ec66a9194acaa66a322b74
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-27 22:54:03 -0500

    s/p/distribution

--- Distribution/Distribution.js
+++ Distribution/Distribution.js
@@ -8,12 +8,12 @@ export default function Distribution({values}) {
 	const hist = histogram().domain(abscisa.domain()).thresholds(abscisa.ticks(20));
 	const bins = hist(values);
 	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([120, 0]);
-	const p = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(120).curve(basis);
+	const distribution = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(120).curve(basis);
 
 	return (
 		<article>
 			<svg width={300} height={120}>
-				<path fill="rgba(0, 0, 255, 0.6)" d={p(bins)} />
+				<path fill="rgba(0, 0, 255, 0.6)" d={distribution(bins)} />
 			</svg>
 		</article>
 	);
