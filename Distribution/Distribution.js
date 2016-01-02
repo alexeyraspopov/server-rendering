12e5a790282baf9bb806abe5c9345f2a7beac233
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-01-01 21:21:50 -0500

    updated <Distribution />
    Width and height now can be received from props

--- Distribution/Distribution.js
+++ Distribution/Distribution.js
@@ -3,18 +3,16 @@ import {histogram, max} from 'd3-array';
 import {linear} from 'd3-scale';
 import {area, basis} from 'd3-shape';
 
-export default function Distribution({values}) {
-	const WIDTH = 300;
-	const HEIGHT = 120;
-	const abscisa = linear().domain([0, 1]).range([0, WIDTH]);
+export default function Distribution({values, width = 320, height = 120}) {
+	const abscisa = linear().domain([0, 1]).range([0, width]);
 	const hist = histogram().domain(abscisa.domain()).thresholds(abscisa.ticks(20));
 	const bins = hist(values);
-	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([HEIGHT, 0]);
-	const distribution = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(HEIGHT).curve(basis);
+	const ordinata = linear().domain([0, max(bins, b => b.length)]).range([height, 0]);
+	const distribution = area().x(b => abscisa(b.x0)).y1(b => ordinata(b.length)).y0(height).curve(basis);
 
 	return (
 		<article>
-			<svg width={WIDTH} height={HEIGHT}>
+			<svg width={width} height={height}>
 				<path fill="rgba(0, 0, 255, 0.6)" d={distribution(bins)} />
 			</svg>
 		</article>
