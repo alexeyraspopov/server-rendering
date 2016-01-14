2f2e7e6656e97bb7470156319cc39838f553416d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-01-14 10:48:34 -0500

    added react-transform

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 export NODE_PATH=node_modules:.
 export NODE_ENV=development
 
-nodemon --exec babel-node -- index.node.js &
+BABEL_ENV=production nodemon --exec babel-node -- index.node.js &
 browserify -r react -r react-dom -o vendor.js
-watchify -e index.web.js -x react -x react-dom -t babelify -g envify -p browserify-hmr -o bundle.js -v
+watchify -e index.web.js -x react -x react-dom -p browserify-hmr -t babelify -g envify -o bundle.js -v
