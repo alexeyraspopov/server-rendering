b12c3c9887ea3f6790c16d47b1401cc6498c5b01
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-01-14 11:19:04 -0500

    added source maps

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@ export NODE_ENV=development
 
 BABEL_ENV=production nodemon --exec babel-node -- index.node.js &
 browserify -r react -r react-dom -o vendor.js
-watchify -e index.web.js -x react -x react-dom -p browserify-hmr -t babelify -g envify -o bundle.js -v
+watchify -e index.web.js -x react -x react-dom -t [ babelify --sourceMapRelative . ] -p browserify-hmr -g envify -o bundle.js -dv
