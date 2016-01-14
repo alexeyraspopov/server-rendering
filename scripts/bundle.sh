c7c4bd7b33038a9af13d3f4f1f79232ed42a5dd7
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-01-14 10:59:01 -0500

    server should keep reading env variables

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -5,6 +5,6 @@ rm -rf bundle
 mkdir bundle
 
 npm run lint
-browserify -e index.node.js -t babelify -g envify --bare -o bundle/server.js
+browserify -e index.node.js -t babelify --bare -o bundle/server.js
 browserify -r react -r react-dom -g envify -o bundle/vendor.js
 browserify -e index.web.js -x react -x react-dom -t babelify -g envify -o bundle/client.js
